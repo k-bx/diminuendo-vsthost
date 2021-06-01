1b0556f218393fc00fee771718e634aedd1062cb
Kon Rybnikov <k-bx@k-bx.com>
2021-06-01 22:32:34 +0300

    Load VST plugin and do nothing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::path::Path;
+use std::sync::{Arc, Mutex};
+
+use vst::host::{Host, PluginLoader};
+use vst::plugin::Plugin;
+
+struct SampleHost;
+
+impl Host for SampleHost {
+    fn automate(&self, index: i32, value: f32) {
+        println!("Parameter {} had its value changed to {}", index, value);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let host = Arc::new(Mutex::new(SampleHost));
+    let path = Path::new("/Library/Audio/Plug-Ins/VST/Addictive Keys.vst/Contents/MacOS/Addictive Keys");
+
+    let mut loader = PluginLoader::load(path, host.clone()).unwrap();
+    let mut instance = loader.instance().unwrap();
+
+    println!("Loaded {}", instance.get_info().name);
+
+    instance.init();
+    println!("Initialized instance!");
+
+    println!("Closing instance...");
 }
